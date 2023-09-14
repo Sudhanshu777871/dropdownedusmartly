c3172b42e26d1b81830caac63e93e67fd23c6ac7
Sudhanshu <ksudhanshu394@gmail.com>
2023-09-14 20:01:09 +0530

    edusmartly dropdown code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Home from "./components/Home";
+import PrivateComp from "./components/PrivateComp";
+import ManageStudents from "./components/ManageStudents";
+import Expenses from "./components/Expenses";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Rotes for sidebar */}
+          <Route element={<PrivateComp />}>
+          <Route path="/" element={<Main />}>
+            <Route path="" element={<Home />}></Route>
+            <Route path="/manage_students" element={<ManageStudents/>}></Route>
+            <Route path="/manage_expenses" element={<Expenses/>}></Route>
+            {/* routes for navbar */}
+            <Route path="/about" element={<h1>About</h1>}></Route>
+          </Route>
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
+    </>
+  );
+}
+
+export default App;
